03ee18d3efa14bd03d67b731eee21b6dad6288b9
Roberto Banbanaste <rbanbanaste@outlook.com>
2020-02-04 11:48:42 -0500

    updated dev

--- src/components/RequestCard.js
+++ src/components/RequestCard.js
@@ -1,12 +1,18 @@
 import React from "react";
+
+// material ui
 import { Paper, Typography } from "@material-ui/core";
+import Button from "@material-ui/core/Button";
 
 export default function Request(props) {
   return (
     <Paper style={{ marginBottom: "30px" }}>
-      <Typography variant="h3">{props.title}</Typography>
+      <Typography variant="h5">{props.title}</Typography>
       <Typography variant="body1">{props.description}</Typography>
       <Typography variant="body1">{props.ready_by}</Typography>
+      <Button variant="contained" color="secondary">
+        View Details
+      </Button>
     </Paper>
   );
 }
