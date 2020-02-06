512ac736830e0d32b7a97ac3a0265be5c8d8bc38
Roberto Banbanaste <rbanbanaste@outlook.com>
2020-02-05 19:02:15 -0500

    added conditional render to navbar links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 
 import { connect } from "react-redux";
 
-
 // material ui
 import { Container, Drawer } from "@material-ui/core";
 
@@ -12,9 +11,7 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
-import Link from "@material-ui/core/Link";
-
-import { Link as RouterLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import { useHistory } from "react-router-dom";
 
@@ -37,59 +34,82 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-
 function NavBar(props) {
   const classes = useStyles();
   const [drawerOpen, setDrawerOpen] = useState(false);
 
   const logout = useHistory();
-  
 
   return (
-
     <>
+      <Drawer open={drawerOpen} PaperProps={{ style: { width: "100%" } }}>
+        <Button
+          color="primary"
+          variant="contained"
+          onClick={() => {
+            setDrawerOpen(!drawerOpen);
+          }}
+        >
+          Close
+        </Button>
+        <Button
+          to="/volunteerDashboardAll"
+          color="secondary"
+          component={Link}
+          onClick={() => {
+            setDrawerOpen(!drawerOpen);
+          }}
+        >
+          All requests
+        </Button>
+        <Button
+          to="/volunteerDashboardPersonal"
+          color="secondary"
+          component={Link}
+          onClick={() => {
+            setDrawerOpen(!drawerOpen);
+          }}
+        >
+          My Requests
+        </Button>
+      </Drawer>
 
-    <Drawer open={drawerOpen} PaperProps={{style: {width: '100%'}}}>
-
-      {!JSON.parse(localStorage.getItem('isBusiness')) && localStorage.getItem('id') && 
-       <> <RouterLink to="/volunteerDashboardAll">All requests</RouterLink>
-        <RouterLink to="/volunteerDashboardPersonal">My Requests</RouterLink></>
-      }
-
-
-    <Button color='primary' variant='contained' onClick={() => {
-                setDrawerOpen(!drawerOpen);
-              }}>Drawer</Button>
-    </Drawer>
-
-    <div className={classes.root}>
-      <AppBar position="static" color="secondary">
-        <Container maxWidth="md">
-          <Grid>
-            <Toolbar>
-              <Typography variant="h4" className={classes.title}>
-                Replate
-              </Typography>
-              <Button color='primary' variant='contained' onClick={() => {
-                setDrawerOpen(!drawerOpen);
-              }}>Drawer</Button>
-              {localStorage.getItem("id") && (
-                <Button
-                  onClick={() => {
-                    localStorage.clear();
-                    logout.push("/");
-                  }}
-                >
-                  Logout
-                </Button>
-              )}
-            </Toolbar>
-          </Grid>
-        </Container>
-      </AppBar>
-    </div>
-
-
+      <div className={classes.root}>
+        <AppBar position="static" color="secondary">
+          <Container maxWidth="md">
+            <Grid>
+              <Toolbar>
+                <Typography variant="h4" className={classes.title}>
+                  Replate
+                </Typography>
+                {localStorage.getItem("id") && (
+                  <Button
+                    style={{ color: "#fff", marginRight: "15px" }}
+                    onClick={() => {
+                      localStorage.clear();
+                      logout.push("/");
+                    }}
+                  >
+                    Logout
+                  </Button>
+                )}
+                {!JSON.parse(localStorage.getItem("isBusiness")) &&
+                  localStorage.getItem("id") && (
+                    <Button
+                      color="primary"
+                      variant="contained"
+                      onClick={() => {
+                        setDrawerOpen(!drawerOpen);
+                      }}
+                    >
+                      +
+                    </Button>
+                  )}
+              </Toolbar>
+            </Grid>
+          </Container>
+        </AppBar>
+      </div>
     </>
   );
 }
