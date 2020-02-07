7110d3444b9807c67bc80132912d023664873fc7
liampmoore <16942895+liampmoore@users.noreply.github.com>
2020-02-06 20:38:14 -0500

    Removed unused imports in navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -11,12 +11,11 @@ import { Container, Drawer } from "@material-ui/core";
 import Hidden from "@material-ui/core/Hidden";
 
 import Grid from "@material-ui/core/Grid";
-import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
-import Link from "@material-ui/core/Link";
+
 
 import IconButton from "@material-ui/core/IconButton";
 import Menu from "@material-ui/icons/Menu";
